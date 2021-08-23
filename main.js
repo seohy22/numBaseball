ff813a0632ab9a7fe690def80701547347ab3556
seohy22 <jingmikim6@gmail.com>
2021-08-24 01:24:35 +0900

    완성전

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const createNum = document.querySelector('#start');
 const inputForm = document.querySelector('#inputForm');
 const inputNums = document.querySelector('#inputNums');
+const ul = document.querySelector('#resultUl');
+const reStartBtn = document.querySelector('#restart');
 const ranNumArr = [];
 const userNumArr = [];
+let cond = false;
 let number;
 let life = 10;
 let strike = 0;
@@ -11,6 +14,9 @@ let ball = 0;
 // 초기번호 생성
 // 번호생성 클릭 시 임의 번호 4자리 생성합니다.
 createNum.addEventListener('click', function(){
+    cond=true;
+    createNum.disabled = true;
+    inputNums.value = null;
     for(let i=0; i<4; i++){
         let ranNum = Math.floor(Math.random() * 10);
         ranNumArr[i] = ranNum;
@@ -35,6 +41,9 @@ createNum.addEventListener('click', function(){
 //예외검증
 //썼던 값 검증안함
 let checkNum = number => {
+    if(cond != true){
+        return alert("시작버튼을 눌러주세요!");
+    }
     if(number.length !== 4){
         return alert("4자리 숫자 입력하세요");
     }
@@ -60,6 +69,7 @@ inputForm.addEventListener('submit',(event) => {
     //표출 후 strike,ball 초기화, life 감소
     if(JSON.stringify(ranNumArr) === JSON.stringify(userNumArr)){
         alert("정답입니다!");
+        life = 10;
     }else{
         for(i=0;i<4;i++){
             if(ranNumArr[i] == userNumArr[i]){
@@ -68,28 +78,32 @@ inputForm.addEventListener('submit',(event) => {
                 ball++;
             }
         }
-        console.log(userNumArr.join('')+ "\t" + strike +"스트라이크 " + ball + "볼");
+        life--;
+        let viewNum = userNumArr.join('');
+        viewResult(viewNum,strike,ball,life);
+        console.log(userNumArr.join('')+ "\t" + strike +"S" + ball + "B");
         ball=0;
         strike=0;
-        life--;
+        if(life==0){
+            inputNums.disabled = true;
+            inputBtn.disabled = true;
+            const li = document.createElement("li");
+            const span = document.querySelector('#life');
+            span.innerHTML = "실패";
+            li.appendChild(document.createTextNode("정답은! "+ranNumArr.join('')+" 입니다"));
+            ul.appendChild(li);
+        }
     }
-
-    if(life==0){
-        alert("실패입니다");
-        life=10;
-    }
-        // for(i=0;i<4;i++){
-        //     if(ranNumArr[i] == Number(number[i])){
-        //         console.log("동일합니다")
-        //     }
-        // }
 })
 
-
-// for (let i =0; i<4;i++){
-//     if(a[i] == b[i]) {
-//         console.log("스트라이크");
-//     }else if(a.includes(b[i])){
-//         console.log("볼");
-//     }
-// }
+//입력 결과 값을 뿌려줍니다 (li)
+const viewResult = (viewNum,strike,ball,life) =>{
+    const span = document.querySelector('#life');
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(viewNum+"\t"+strike+"S"+" "+ball+"B"));
+    ul.appendChild(li);
+    span.innerHTML = life+"회";
+}
+reStartBtn.addEventListener('click',() => {
+    location.reload(true);
+});
